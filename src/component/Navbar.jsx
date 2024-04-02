f5421a858081a56d7e6e8b9ccfb92f4887ba73bb
Achintha Gamlath <90699843+ayugamlath@users.noreply.github.com>
2024-04-02 12:59:36 +0530

    navbar updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,27 +1,20 @@
 import React from 'react'
 import './Navbar.css'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { HomeOutlined } from '@ant-design/icons';
-import dash from '../assets/dash.svg';
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
 function navbar() {
   const isCurrentLink = (path) => window.location.pathname === path;
   return (
-    <div className='w-25 bg-info'>
+    <div className='w-25 bg-info bg-gradient' >
       <div className='w-100'>
         <div className=' w-100'>
           <div className=' col-auto col-md-2 min-vh-100 w-100'>
-            <a className='text-decoration-none text-white d-flex align-itemcenter' style={{ marginLeft: '40px', marginTop: '2rem' }}>
+            <a className='text-decoration-none text-white d-flex align-itemcenter m-lg-5 ' >
 
               <span className='ms-1 fs-4'>Realync</span>
             </a>
             <br />
-            <ul className="nav nav-pills flex-column mt-3 mt-sm-0">
+            <ul className="nav nav-pills flex-column mt-3 mt-sm-0 ms-5 ">
               <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
                 <a href="#" className="nav-link text-white  fs-5 d-flex" aria-current="page">
                   <i className='bi bi-speedometer2'></i>
@@ -32,7 +25,7 @@ function navbar() {
 
               <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
                 <a href="#" className="nav-link text-white  fs-5 d-flex" aria-current="page">
-                  <i className='bi bi-speedometer2'></i>
+                  <i className='bi bi-calendar4-week'></i>
                   <div className="opa">
                     <span className='ms-3 d-none d-sm-inline'>Tours</span></div>
                 </a>
@@ -40,9 +33,23 @@ function navbar() {
 
               <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
                 <a href="#" className="nav-link text-white  fs-5 d-flex" aria-current="page">
-                  <i className='bi bi-speedometer2'></i>
+                  <i className='bi bi-file-ruled'></i>
                   <div className="opa">
                     <span className='ms-3 d-none d-sm-inline'>Properties</span></div>
+                </a>
+              </li>
+              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
+                <a href="#" className="nav-link text-white  fs-5 d-flex" aria-current="page">
+                  <i className='bi bi-envelope'></i>
+                  <div className="opa">
+                    <span className='ms-3 d-none d-sm-inline'>Messages</span></div>
+                </a>
+              </li>
+              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
+                <a href="#" className="nav-link text-white  fs-5 d-flex" aria-current="page">
+                  <i className='bi bi-gear'></i>
+                  <div className="opa">
+                    <span className='ms-3 d-none d-sm-inline'>Settings</span></div>
                 </a>
               </li>
 
